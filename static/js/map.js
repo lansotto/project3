d1ad35f2bdd9181f61a8beff1a17ccdfbdf2cc2a
lansotto <49457876+lansotto@users.noreply.github.com>
2023-04-23 16:14:02 -0400

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,7 @@
 csv = "static/data/tmdb_5000_movies_countries.csv";
 d3.csv(csv).then(createMarkers);
 
-function createMap(movies)
-{
+function createMap(movies) {
     var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
@@ -40,22 +39,23 @@ function createMap(movies)
     // create baseMaps object
     var baseMaps = {
         "Street Map": streetmap,
-        'MIERUNE Color': m_color,
-        'MIERUNE MONO': m_mono,
+        'Mierune Colour': m_color,
+        'Mierune Mono': m_mono,
+        'GSI Ort': t_ort,
+        //  'OSM': o_std,
+        'GSI Pale': t_pale,
     };
 
     // create overlayMaps object
     var overlayMaps = {
-        "Movies": movies,
-        'OSM': o_std,
-        'GSI Pale': t_pale,
-        'GSI Ort': t_ort,
+        "Production Countries": movies,
+
     };
 
     var map = L.map("map", {
         center: [38.9637, 35.2433],
         zoom: 2,
-        layers:[streetmap,m_mono, o_std, t_pale, t_ort, movies]
+        layers: [streetmap, m_mono, o_std, t_pale, t_ort, movies]
     });
 
     L.control.layers(baseMaps, overlayMaps, {
@@ -63,41 +63,57 @@ function createMap(movies)
     }).addTo(map);
 
     //OpacityControl
-    L.control
-    .opacity(overlayMaps, {
-        label: 'Layers Opacity',
-    })
-    .addTo(map);
+    // L.control
+    //     .opacity(overlayMaps, {
+    //         label: 'Layers Opacity',
+    //     })
+    //     .addTo(map);
 
-    // Create a legend -  https://gis.stackexchange.com/questions/133630/adding-leaflet-legend 
-    let getColor = ["red", "orangered", "orange", "yellow", "greenyellow", "limegreen"];
 
-    let legend = L.control({position: 'bottomright'});
-        legend.onAdd = function () {
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function () {
 
-    let div = L.DomUtil.create('div', 'info legend');
-    let labels = ["<strong>Movies Count</strong>"];
-    let categories = ['0-50', '50-100', '100-250', '250-500', '500-1000', '>1000'];
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 100, 200, 300, 1000, 2000]
+        labels = [];
 
-    for (let i = 0; i < categories.length; i++) {
-            div.innerHTML += 
-            labels.push(
-                        '<li class="circle" style="background-color:' + getColor[i] + '">' + categories[i] + '</li> '
-                    );
-            }
-            div.innerHTML = '<ul style="list-style-type:none; text-align: center">' + labels.join('') + '</ul>'
-            return div;
-        };
+        // Generate label 
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<li style="background-color:' + setColor(grades[i] + 1) + '"></li> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
     legend.addTo(map);
 }
+function setColor(depth) {
+    if (depth < 100) {
+        color = '#9ee053';
+    }
+    else if (depth < 200) {
+        color = '#d1e36b';
+    }
+    else if (depth < 300) {
+        color = '#faf328';
+    }
+    else if (depth < 1000) {
+        color = '#f0cc4a';
+    }
+    else if (depth < 2000) {
+        color = '#f0a04a';
+    }
+    else {
+        color = '#f0694a';
+    }
+    return color;
+}
 
-function createMarkers(response)
-{
+function createMarkers(response) {
     var movie_data = response;
     var moviemarkers = [];
-      
-    for(var index = 0; index < movie_data.length; index++)
-    {
+
+    for (var index = 0; index < movie_data.length; index++) {
         var prodCountry = movie_data[index];
         var depth = prodCountry.Count;
         var magnitude = prodCountry.Count;
@@ -105,50 +121,20 @@ function createMarkers(response)
             radius: setRadius(magnitude),
             color: "#000",
             weight: 1,
-            opacity: .75,
+            opacity: .5,
             fillOpacity: 0.8,
             fillColor: setColor(depth),
-         };
-        var moviemarker = L.circle([prodCountry.latitude,prodCountry.longitude], markeroptions)
-            .bindPopup("<h3> Production Country: " + prodCountry.Country + "<br> Number of films: " + prodCountry.Count + "</h3>");
-            moviemarkers.push(moviemarker);
+        };
+        var moviemarker = L.circle([prodCountry.latitude, prodCountry.longitude], markeroptions)
+            .bindPopup("<body> Production Country: <b>" + prodCountry.Country + "</b><br> Number of films: <b>" + prodCountry.Count + "</b></body>");
+        moviemarkers.push(moviemarker);
 
     }
     const mapLayer = L.layerGroup(moviemarkers);
     createMap(mapLayer);
 }
 
-function setRadius(magnitude)
-{
+function setRadius(magnitude) {
     // set size of markers
     return magnitude * 400;
-}
-
-function setColor(depth)
-{
-    if(depth < 10)
-    {
-        color = '#9ee053';
-    }
-    else if (depth < 30)
-    {
-        color = '#d1e36b';
-    }
-    else if (depth < 50)
-    {
-        color = '#faf328';
-    }
-    else if (depth < 70)
-    {
-        color = '#f0cc4a';
-    }
-    else if (depth < 90)
-    {
-        color = '#f0a04a';
-    }
-    else
-    {
-        color = '#f0694a';
-    }
-    return color;
 }
